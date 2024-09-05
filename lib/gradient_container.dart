7f08d6a1cb81f8aca3ef43f1c03c03b9f89f07cc
vincent anjiri <vincentanjiri12@gmail.com>
2024-09-05 16:37:35 +0300

    "Updated GradientContainer to StatefulWidget, added rollDice function, and modified its child widget to include an image and a TextButton. Also, added assets to pubspec.yaml."

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:dice_roll_fluter_app/style_text.dart';
+// import 'package:dice_roll_fluter_app/style_text.dart';
 
 const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.topRight;
 const colorOne = Color.fromARGB(255, 26, 2, 80);
 const colorTwo = Color.fromARGB(255, 45, 7, 98);
+var activeDiceImage = 'assests/dice-images/dice-1.png';
 
-class GradientContainer extends StatelessWidget {
+void rollDice() {
+  activeDiceImage = 'assests/dice-images/dice-4.png';
+  print('changing image');
+}
+
+class GradientContainer extends StatefulWidget {
   const GradientContainer({super.key});
 
   @override
@@ -18,7 +24,28 @@ class GradientContainer extends StatelessWidget {
           colorTwo,
         ], begin: startAlignment, end: endAlignment),
       ),
-      child: const Center(child: StyleText('Hello Flutter Devs')),
+      child: Center(
+          child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            activeDiceImage,
+            width: 200,
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          TextButton(
+            onPressed: rollDice,
+            style: TextButton.styleFrom(
+              // padding: const EdgeInsets.only(top: 20),
+              foregroundColor: Colors.white,
+              textStyle: const TextStyle(fontSize: 28),
+            ),
+            child: const Text('Roll Dice'),
+          )
+        ],
+      )),
     );
   }
 }
