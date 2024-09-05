8369ff1541869b20e944322710857e2f3422f29d
vincent anjiri <vincentanjiri12@gmail.com>
2024-09-05 14:31:50 +0300

    "Updated main.dart to use MaterialApp with Scaffold and GradientContainer, replaced Text widget with a Container having a gradient background and centered text."

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,34 @@
-import 'package:/flutter/material.dart';
+import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Text('Hello World! '),
-  ));
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: GradientContainer(),
+      ),
+    ),
+  );
+}
+
+class GradientContainer extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(colors: [
+          Color.fromARGB(255, 26, 2, 80),
+          Color.fromARGB(255, 45, 7, 98),
+        ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+      ),
+      child: const Center(
+        child: Text(
+          'Mambo yangu ni ngumu😂😂',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 28,
+          ),
+        ),
+      ),
+    );
+  }
 }
